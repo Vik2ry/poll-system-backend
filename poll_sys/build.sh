f012ccf40435911aa0d34e968644f324628de294
Olusegun Banji <41373128+Vik2ry@users.noreply.github.com>
2025-08-02 11:15:37 +0100

    removed super user instance

--- poll_sys/build.sh
+++ poll_sys/build.sh
@@ -7,13 +7,13 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python manage.py createsuperuser \
-        --no-input \
-        --username "$DJANGO_SUPERUSER_USER" \
-        --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python manage.py createsuperuser \
+#         --no-input \
+#         --username "$DJANGO_SUPERUSER_USER" \
+#         --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
 
 # Finally, start the server
 exec python manage.py runserver 0.0.0.0:8000
